33a60fa81508dd9786b6f5d4b033af09200a002d
Trent Moss <trentmoss05@gmail.com>
2019-03-15 17:24:47 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -24,7 +24,7 @@ class SongsController < ApplicationController
     @song.save
 
     flash[:message] = "Successfully created song."
-    redirect to "/songs/#{@song.slug}"
+    redirect("/songs/#{@song.slug}")
   end
 
   get '/songs/:slug/edit' do
