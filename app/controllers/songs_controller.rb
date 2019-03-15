6f428267cd133b163ff76f4b81b370241fdf6910
Trent Moss <trentmoss05@gmail.com>
2019-03-15 17:24:11 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -26,4 +26,20 @@ class SongsController < ApplicationController
     flash[:message] = "Successfully created song."
     redirect to "/songs/#{@song.slug}"
   end
+
+  get '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'songs/edit'
+  end
+
+  patch '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    @song.update(params[:song])
+    @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+    @song.genre_ids = params[:genres]
+    @song.save
+
+    flash[:message] = "Successfully updated song."
+    redirect("/songs/#{@song.slug}")
+  end
 end
